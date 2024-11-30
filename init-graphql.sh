218d4ba2debd37a36ef8ebf6c3275f47ae571262
smazouz42 <smazouz@student.1337.ma>
2024-11-30 07:26:19 -0800

    fix: remove styles.8f1d28c5983cf8f0.css.gz after unzip to avoid any errors

--- init-graphql.sh
+++ init-graphql.sh
@@ -2,7 +2,7 @@
 rm -f styles.8f1d28c5983cf8f0.css
 
 gunzip -k styles.8f1d28c5983cf8f0.css.gz
-
+rm -f  styles.8f1d28c5983cf8f0.css.gz
 echo "body { background: gray; padding: 20px; }" >> styles.8f1d28c5983cf8f0.css
 
 gzip styles.8f1d28c5983cf8f0.css
